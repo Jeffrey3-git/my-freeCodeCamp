3343d423d5442c9836c1ea352361b844737fdbf4
Jeffrey3-git <jeffakubea@gmail.com>
2026-06-01 14:29:58 +0000

    feat(lookup): Implement contact profile lookup function
    Added a function to look up contact profiles by name and property.

---
+++ Labs/profileLookup.js
@@ -0,0 +1,45 @@
+let contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name,property){
+  for (const info of contacts){
+    if (info.firstName == name){
+      if (info[property] !== undefined){
+        return info[property];
+      } else {
+        return "No such property"
+      }
+    }
+  }
+  return "No such contact";
+}
+console.log(lookUpProfile("Kristian", "lastName"));
+console.log(lookUpProfile("Sherlock", "likes"));
+console.log(lookUpProfile("Harry", "likes"));
+console.log(lookUpProfile("Bob", "number"));
+console.log(lookUpProfile("Akira", "address"));
+
