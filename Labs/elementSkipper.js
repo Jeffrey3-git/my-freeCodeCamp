fb9bf054ec69febbb722825e5d158c2ea1f8699a
Jeff Akubea <jeffakubea@gmail.com>
2026-06-09 11:06:41 +0000

    Add dropElements function to filter array elements

---
+++ Labs/elementSkipper.js
@@ -0,0 +1,15 @@
+function dropElements(arr, func){
+  let index = arr.findIndex(ind => func(ind));
+  return index != -1 ? arr.slice(index):[];
+}
+
+console.log(dropElements([1, 2, 3, 4], function(n) {return n >= 3;}));
+console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;}));
+console.log(dropElements([1, 2, 3], function(n) {return n > 0;}));
+console.log(dropElements([1, 2, 3, 4], function(n) {return n > 5;}));
+console.log(dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}));
+console.log(dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}));
+
+
+ 
+
