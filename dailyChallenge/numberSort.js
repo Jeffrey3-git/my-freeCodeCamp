8c49696a1743c24fffb6c70ddbb007a7e82e4c26
Jeff Akubea <jeffakubea@gmail.com>
2026-06-15 10:39:57 +0000

    feat(num-sort): Implement number sorting function in numberSort.js
    The sortNumbers function takes a string of numbers as parameter abd returns an array of the numbers in sorted order.

---
+++ dailyChallenge/numberSort.js
@@ -0,0 +1,13 @@
+function sortNumbers(str) {
+  let numbers = str.split(",");
+  let arr = [];
+  for (const number of numbers){
+    arr.push(parseInt(number));
+  }
+  return arr.sort((a,b) => a-b);
+}
+
+console.log(sortNumbers("3,1,2"));
+console.log(sortNumbers("5,3,8,1,9,2"));
+console.log(sortNumbers("12,61,49,80,19,50,77,38"));
+console.log(sortNumbers("0,6,-19,44,-2,7,0"));
