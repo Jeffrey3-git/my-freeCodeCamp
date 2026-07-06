ccea29fc604958a711dddcad505c7053c85e7c05
Jeff Akubea <jeffakubea@gmail.com>
2026-07-06 11:44:32 +0000

    Add oddOrEven function to determine parity

---
+++ dailyChallenge/oddOrEven.js
@@ -0,0 +1,9 @@
+function oddOrEven(n) {
+  return n%2 === 0 ? 'Even' : 'Odd';
+}
+
+console.log(oddOrEven(1));
+console.log(oddOrEven(2));
+console.log(oddOrEven(13));
+console.log(oddOrEven(196));
+console.log(oddOrEven(123456789));
